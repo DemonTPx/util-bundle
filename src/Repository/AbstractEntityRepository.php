d046e801d90d722bfbb81651a473ab8cf47ce69c
Bert Hekman <demontpx@gmail.com>
2018-12-05 00:06:55 +0100

    Add AbstractEntityRepository

---
+++ src/Repository/AbstractEntityRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Demontpx\UtilBundle\Repository;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * @copyright 2018 Bert Hekman
+ */
+abstract class AbstractEntityRepository
+{
+    /** @var EntityRepository */
+    protected $repository;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->repository = $entityManager->getRepository($this->getClassName());
+    }
+
+    abstract protected function getClassName(): string;
+}
