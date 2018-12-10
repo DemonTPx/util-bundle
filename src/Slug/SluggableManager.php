56287e0b8a45b637126785da09b53d91953fd64f
Bert Hekman <demontpx@gmail.com>
2018-12-10 21:02:27 +0100

    Sluggable configuration can now be fetched for a class's parents as well

--- src/Slug/SluggableManager.php
+++ src/Slug/SluggableManager.php
@@ -28,6 +28,16 @@ class SluggableManager
             throw new \InvalidArgumentException(sprintf('Class %s should implement %s', $class, SluggableInterface::class));
         }
 
-        return $this->configurationList[$class];
+        if (isset($this->configurationList[$class])) {
+            return $this->configurationList[$class];
+        }
+
+        foreach (class_parents($class) as $parent) {
+            if (isset($this->configurationList[$parent])) {
+                return $this->configurationList[$parent];
+            }
+        }
+
+        throw new \InvalidArgumentException(sprintf('No sluggable configuration found for class %s or its parent(s) ', $class));
     }
 }
